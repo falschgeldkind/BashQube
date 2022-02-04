f8abbee16971ccaad2243a94242e17eba90864ca
christian_bley <christian.bley@tu-braunschweig.de>
2022-02-04 22:34:57 +0100

    changed names

--- automated/01_auto-basic-config-example.sh
+++ automated/01_auto-basic-config-example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #*Versions: 1a 2b 3c
-#*Name: matlab
+#*Name: basic
 
 echo "This is an example script for an automated basic configuration step!"
-echo "The selected version of Matlab is: $matlabVer"
+echo "The selected version of Matlab is: $basicVer"
 sleep 10
