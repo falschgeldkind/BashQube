2affa51728c8d344c1eb5cb8e74f87580ee00dc0
christian_bley <christian.bley@tu-braunschweig.de>
2022-02-04 22:23:34 +0100

    some more of that

--- automated/01_auto-basic-config-example.sh
+++ automated/01_auto-basic-config-example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #*Versions: 1a 2b 3c
+#*Name: matlab
 
 echo "This is an example script for an automated basic configuration step!"
 sleep 10
--- automated/13_auto-non-basic-example.sh
+++ automated/13_auto-non-basic-example.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #*Versions: 1 2 3
+#*Name: Matlab
 
 echo "This is an example script for an automated non-basic configuration step!"
 sleep 10
